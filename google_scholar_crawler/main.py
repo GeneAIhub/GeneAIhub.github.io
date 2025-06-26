f6b85f961db3fe5a4280edcfc0b7e254bae7bb92
WANGTENG <wangteng1009@gmail.com>
2025-06-26 21:00:11 +0800

    update the cv 11

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,32 +1,12 @@
-import os
-print("Running main.py")
-print("GOOGLE_SCHOLAR_ID =", os.getenv("GOOGLE_SCHOLAR_ID"))
 from scholarly import scholarly
 import jsonpickle
 import json
 from datetime import datetime
+import os
 
-
-
-# Checking if GOOGLE_SCHOLAR_ID is set
-google_scholar_id = os.getenv("GOOGLE_SCHOLAR_ID")
-if not google_scholar_id:
-    print("GOOGLE_SCHOLAR_ID is not set!")
-    exit(1)
-
-print("Running main.py")
-print("GOOGLE_SCHOLAR_ID =", google_scholar_id)
-
-# Try fetching the author data
-try:
-    author = scholarly.search_author_id(google_scholar_id)
-    scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-except Exception as e:
-    print(f"Error occurred while fetching author data: {e}")
-    exit(1)
-
-
-# name = author['name']
+author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+name = author['name']
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
 print(json.dumps(author, indent=2))
@@ -39,9 +19,5 @@ shieldio_data = {
   "label": "citations",
   "message": f"{author['citedby']}",
 }
-print("Running main.py")
-print("GOOGLE_SCHOLAR_ID =", os.getenv("GOOGLE_SCHOLAR_ID"))
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
-
-print("Data saved successfully!")
